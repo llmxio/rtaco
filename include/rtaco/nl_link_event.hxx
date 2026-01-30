195f0230ebafd3c881c2580512fb5f456d5c45fc
Roman Stolyarchuk <roman@stolyarch.uk>
2026-01-30 13:33:42 +0300

    feat: update LinkEvent flags to use bitwise operators and add support for bitmask - Refactor Flags enum to use bitwise shift operators for clarity - Add enable_bitmask_operators specialization for Flags enum

--- include/rtaco/nl_link_event.hxx
+++ include/rtaco/nl_link_event.hxx
@@ -6,6 +6,8 @@
 
 #include <linux/rtnetlink.h>
 
+#include "rtaco/nl_utils.hxx"
+
 struct nlmsghdr;
 
 namespace llmx {
@@ -19,26 +21,26 @@ struct LinkEvent {
     };
 
     enum class Flags : uint32_t {
-        UNKNOWN = 0,
-        UP = 1 << 0,
-        BROADCAST = 1 << 1,
-        DEBUG = 1 << 2,
-        LOOPBACK = 1 << 3,
-        POINTOPOINT = 1 << 4,
-        NOTRAILERS = 1 << 5,
-        RUNNING = 1 << 6,
-        NOARP = 1 << 7,
-        PROMISC = 1 << 8,
-        ALLMULTI = 1 << 9,
-        MASTER = 1 << 10,
-        SLAVE = 1 << 11,
-        MULTICAST = 1 << 12,
-        PORTSEL = 1 << 13,
-        AUTOMEDIA = 1 << 14,
-        DYNAMIC = 1 << 15,
-        LOWER_UP = 1 << 16,
-        DORMANT = 1 << 17,
-        ECHO = 1 << 18,
+        UNKNOWN = 0,             // 0x00000000
+        UP = (1u << 0),          // 0x00000001
+        BROADCAST = (1u << 1),   // 0x00000002
+        DEBUG = (1u << 2),       // 0x00000004
+        LOOPBACK = (1u << 3),    // 0x00000008
+        POINTOPOINT = (1u << 4), // 0x00000010
+        NOTRAILERS = (1u << 5),  // 0x00000020
+        RUNNING = (1u << 6),     // 0x00000040
+        NOARP = (1u << 7),       // 0x00000080
+        PROMISC = (1u << 8),     // 0x00000100
+        ALLMULTI = (1u << 9),    // 0x00000200
+        MASTER = (1u << 10),     // 0x00000400
+        SLAVE = (1u << 11),      // 0x00000800
+        MULTICAST = (1u << 12),  // 0x00001000
+        PORTSEL = (1u << 13),    // 0x00002000
+        AUTOMEDIA = (1u << 14),  // 0x00004000
+        DYNAMIC = (1u << 15),    // 0x00008000
+        LOWER_UP = (1u << 16),   // 0x00010000
+        DORMANT = (1u << 17),    // 0x00020000
+        ECHO = (1u << 18),       // 0x00040000
     };
 
     Type type{Type::UNKNOWN};
@@ -51,6 +53,9 @@ struct LinkEvent {
 };
 
 using LinkEventList = std::pmr::vector<LinkEvent>;
+
+template<>
+struct enable_bitmask_operators<LinkEvent::Flags> : std::true_type {};
 
 } // namespace rtaco
 } // namespace llmx
